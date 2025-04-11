9c0724f164f93e13578d3f301e12ec3978cc73ed
Diogo <dv.jesus@campus.fct.unl.pt>
2025-04-11 15:01:09 +0100

    Op 2 - Done

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/AuthToken.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/AuthToken.java
@@ -1,25 +1,28 @@
 package pt.unl.fct.di.apdc.firstwebapp.util;
 
+import com.google.cloud.Timestamp;
+import java.util.Date;
 import java.util.UUID;
 
 public class AuthToken {
 
-	public static final long EXPIRATION_TIME = 1000*60*60*2;
-	
-	public String username;
-	public String tokenID;
-	public long creationData;
-	public long expirationData;
-	
-	public AuthToken() {
+    public static final long EXPIRATION_TIME = 1000 * 60 * 60 * 2; // 2 hours in ms
 
-	}
-	
-	public AuthToken(String username) {
-		this.username = username;
-		this.tokenID = UUID.randomUUID().toString();
-		this.creationData = System.currentTimeMillis();
-		this.expirationData = this.creationData - EXPIRATION_TIME;
-	}
-	
+    public String username;    
+    public String tokenID;     
+    public Timestamp validFrom;     
+    public Timestamp validTo;       
+    public String role;           
+
+    public AuthToken() {
+    }
+
+    public AuthToken(String username, String role) {
+        this.username = username;
+        this.role = role;
+        this.tokenID = UUID.randomUUID().toString();
+        this.validFrom = Timestamp.now();
+        long expirationMillis = this.validFrom.toDate().getTime() + EXPIRATION_TIME;
+        this.validTo = Timestamp.of(new Date(expirationMillis));
+    }
 }
