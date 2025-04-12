a9e80a4afd3d4d43039a9dbdd163c921bbd5b508
Diogo <dv.jesus@campus.fct.unl.pt>
2025-04-12 01:10:59 +0100

    Altered the Login function so that only one token will be created per user and it will update the existing token instead

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
@@ -1,15 +1,20 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
+import java.util.Date;
 import java.util.logging.Logger;
 import java.net.URI;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import com.google.cloud.Timestamp;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
 import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -34,7 +39,7 @@ public class LoginResource {
 
 	private static final String USER_PWD = "user_pwd";
 	private static final String USER_ROLE = "user_role";
-	
+
 	private static final String TOKEN_USER = "token_user";
 	private static final String TOKEN_ROLE = "token_role";
 	private static final String TOKEN_EMISSION = "token_emission";
@@ -47,43 +52,64 @@ public class LoginResource {
 	public LoginResource() {
 
 	}
-	
+
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response doLogin(LoginData data) {
-	    LOG.fine(LOG_MESSAGE_LOGIN_ATTEMP + data.username);
+		LOG.fine(LOG_MESSAGE_LOGIN_ATTEMP + data.username);
 
-	    Key userKey = userKeyFactory.newKey(data.username);
-	    Entity user = datastore.get(userKey);
+		Key userKey = userKeyFactory.newKey(data.username);
+		Entity user = datastore.get(userKey);
+		if (user == null) {
+			LOG.warning(LOG_MESSAGE_UNKNOW_USER + data.username);
+			return Response.status(Status.FORBIDDEN).entity(MESSAGE_INVALID_CREDENTIALS).build();
+		}
 
-	    if (user == null) {
-	        LOG.warning(LOG_MESSAGE_UNKNOW_USER + data.username);
-	        return Response.status(Status.FORBIDDEN).entity(MESSAGE_INVALID_CREDENTIALS).build();
-	    }
+		String hashedStoredPassword = user.getString(USER_PWD);
+		if (!hashedStoredPassword.equals(DigestUtils.sha512Hex(data.password))) {
+			LOG.warning(LOG_MESSAGE_WRONG_PASSWORD + data.username);
+			return Response.status(Status.FORBIDDEN).entity(MESSAGE_INVALID_CREDENTIALS).build();
+		}
 
-	    String hashedStoredPassword = user.getString(USER_PWD);
-	    if (!hashedStoredPassword.equals(DigestUtils.sha512Hex(data.password))) {
-	        LOG.warning(LOG_MESSAGE_WRONG_PASSWORD + data.username);
-	        return Response.status(Status.FORBIDDEN).entity(MESSAGE_INVALID_CREDENTIALS).build();
-	    }
+		Query<Entity> tokenQuery = Query.newEntityQueryBuilder().setKind("AuthToken")
+				.setFilter(StructuredQuery.PropertyFilter.eq(TOKEN_USER, data.username)).build();
+		QueryResults<Entity> tokenResults = datastore.run(tokenQuery);
 
-	    AuthToken token = new AuthToken(data.username, user.getString(USER_ROLE));
+		AuthToken token;
+		Entity tokenEntity;
 
-	    Key tokenKey = datastore.newKeyFactory().setKind("AuthToken").newKey(token.tokenID);
-	    Entity tokenEntity = Entity.newBuilder(tokenKey)
-	            .set(TOKEN_USER, token.username)
-	            .set(TOKEN_ROLE, token.role)
-	            .set(TOKEN_EMISSION, token.validFrom)
-	            .set(TOKEN_EXPIRATION, token.validTo)
-	            .build();
-	    datastore.put(tokenEntity);
+		if (tokenResults.hasNext()) {
+			
+			Timestamp newValidFrom = Timestamp.now();
+			long newExpirationMillis = newValidFrom.toDate().getTime() + AuthToken.EXPIRATION_TIME;
+			Timestamp newValidTo = Timestamp.of(new Date(newExpirationMillis));
+			
+			tokenEntity = tokenResults.next();
+			Key tokenKey = tokenEntity.getKey();
+			token = new AuthToken();
+			token.username = data.username;
+			token.role = user.getString(USER_ROLE);
+			token.tokenID = tokenKey.getName();
+			token.validFrom = newValidFrom;
+			token.validTo = newValidTo;
 
-	    LOG.info(LOG_MESSAGE_LOGIN_SUCCESSFUL + data.username);
-	    URI redirectUri = URI.create("/welcome/welcome.html?user=" + token.username + "&role=" + token.role);
-	    return Response.temporaryRedirect(redirectUri).build();
+			tokenEntity = Entity.newBuilder(tokenKey).set(TOKEN_USER, token.username).set(TOKEN_ROLE, token.role)
+					.set(TOKEN_EMISSION, token.validFrom).set(TOKEN_EXPIRATION, token.validTo).build();
+			datastore.put(tokenEntity);
+		} else {
+			token = new AuthToken(data.username, user.getString(USER_ROLE));
+			Key tokenKey = datastore.newKeyFactory().setKind("AuthToken").newKey(token.tokenID);
+			tokenEntity = Entity.newBuilder(tokenKey).set(TOKEN_USER, token.username).set(TOKEN_ROLE, token.role)
+					.set(TOKEN_EMISSION, token.validFrom).set(TOKEN_EXPIRATION, token.validTo).build();
+			datastore.put(tokenEntity);
+		}
 
+		LOG.info(LOG_MESSAGE_LOGIN_SUCCESSFUL + data.username);
+
+		URI redirectUri = URI.create("/welcome/welcome.html?user=" + data.username + "&role=" + token.role);
+		return Response.temporaryRedirect(redirectUri).build();
 	}
-	
+
 }
